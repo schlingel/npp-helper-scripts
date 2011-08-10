cfdc94ac4cbb8f3b327077bceac6f4b838cbb89f
unknown <zombie@.(none)>
2011-08-10 09:07:50 +0200

    Changed the template loader to a snippet functionality

--- LoadTemplate.py
+++ LoadTemplate.py
@@ -5,7 +5,7 @@ import os
 
 # A very simple template insertion script. Underneath is directory where the 
 # templates should go. Templates must have a '.tmplt' postfix e.g. 'test.tmplt'
-# The template insertion script clears the whole file and adds every line from the
+# The template insertion script replaces the selection with every line from the
 # template file.
 #
 # To include a template just enter the name of the template without its postfix and
@@ -16,10 +16,11 @@ selected = editor.getSelText()
 wholePath = os.path.join(templateDir, selected + '.tmplt')
 
 if(os.path.isfile(wholePath)):
-	editor.clearAll()
+	text = ''
 	file = open(wholePath, 'r')
 	for line in file:
-		editor.addText(line)
+		text += line
+	editor.replaceSel(text)
 else:
 	notepad.messageBox('There\'s no template "' + selected + '.tmplt" in ' + templateDir, 'Template not found')
 
